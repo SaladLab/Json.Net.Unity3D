24262a8e7be26d6582cfaf0c7e6486aea7129b5d
Esun Kim <veblush@gmail.com>
2016-04-24 09:58:12 +0900

    Add UnityTest

---
+++ src/UnityTest/CopyFiles.bat
@@ -0,0 +1,6 @@
+COPY /Y ..\Newtonsoft.Json.Tests\SpaceShipV2.bson .\
+COPY /Y ..\Newtonsoft.Json\bin\Release\Newtonsoft.Json.dll .\Assets\
+COPY /Y ..\Newtonsoft.Json\bin\Release\Newtonsoft.Json.dll.mdb .\Assets\
+COPY /Y ..\Newtonsoft.Json.Tests\bin\Release\Newtonsoft.Json.Tests.dll .\Assets\Editor\
+COPY /Y ..\Newtonsoft.Json.Tests\bin\Release\Newtonsoft.Json.Tests.dll.mdb .\Assets\Editor\
+PAUSE.
