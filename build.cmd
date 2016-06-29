c27ea7dba42764edfa3ff7c3a656c959e16395f6
Esun Kim <veblush@gmail.com>
2016-06-29 23:03:33 +0900

    Update package settings

--- build.cmd
+++ build.cmd
@@ -6,16 +6,16 @@ SET PACKAGEPATH=.\packages\
 SET NUGET=.\tools\nuget\NuGet.exe
 SET NUGETOPTIONS=-ConfigFile .\tools\nuget\NuGet.Config -OutputDirectory %PACKAGEPATH% -ExcludeVersion
 
-IF NOT EXIST %PACKAGEPATH%FAKE\Ver_4.25.4 (
+IF NOT EXIST %PACKAGEPATH%FAKE\Ver_4.28.0 (
   RD /S/Q %PACKAGEPATH%FAKE
-  %NUGET% install FAKE -Version 4.25.4 %NUGETOPTIONS%
-  COPY NUL %PACKAGEPATH%FAKE\Ver_4.25.4
+  %NUGET% install FAKE -Version 4.28.0 %NUGETOPTIONS%
+  COPY NUL %PACKAGEPATH%FAKE\Ver_4.28.0
 )
 
-IF NOT EXIST %PACKAGEPATH%FAKE.BuildLib\Ver_0.3.0 (
+IF NOT EXIST %PACKAGEPATH%FAKE.BuildLib\Ver_0.3.6 (
   RD /S/Q %PACKAGEPATH%FAKE.BuildLib
-  %NUGET% install FAKE.BuildLib -Version 0.3.0 %NUGETOPTIONS%
-  COPY NUL %PACKAGEPATH%FAKE.BuildLib\Ver_0.3.0
+  %NUGET% install FAKE.BuildLib -Version 0.3.6 %NUGETOPTIONS%
+  COPY NUL %PACKAGEPATH%FAKE.BuildLib\Ver_0.3.6
 )
 
 set encoding=utf-8
